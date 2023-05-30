7cbd7d39fa960590f6b753d2564e817a9ff15c48
saba <sabaphruidze01@gmail.com>
2023-05-30 14:48:46 +0400

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,66 @@
 import "./App.css";
 import batumi from "./assets/batumi.jpg";
-import hunhary from "./assets/hungary parliment.jpg";
-import moscow from "./assets/Moscow-city,_riverfront_view(31631549126).jpg";
+import hungary from "./assets/hungary.jpg";
+import moscow from "./assets/moscow.jpg";
 import paris from "./assets/paris.jpg";
 import taiwan from "./assets/taiwan.jpg";
 import viena from "./assets/viena.jpg";
 function App() {
+  const data = [
+    {
+      content: "Batumi",
+      img: batumi,
+    },
+    {
+      content: "Hungary",
+      img: hungary,
+    },
+    {
+      content: "Paris",
+      img: paris,
+    },
+    {
+      content: "Moscow",
+      img: moscow,
+    },
+    {
+      content: "Taiwan",
+      img: taiwan,
+    },
+    {
+      content: "Viena",
+      img: viena,
+    },
+  ];
   return (
     <>
-      <div></div>
+      <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          flexDirection: "row",
+          overflow: "hidden",
+          alignItems: "center",
+          justifyContent: " center",
+        }}
+      >
+        {data.map((item: any, idx: number) => (
+          <div
+            key={idx}
+            style={{
+              width: 100,
+              height: 600,
+              backgroundImage: `url(${item.img})`,
+              marginRight: 50,
+              borderRadius: 100,
+              overflow: "hidden",
+            }}
+          >
+            <h4>{item.content}</h4>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
