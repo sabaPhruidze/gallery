89894f03d9f62c19f68e25b6a22068980c8ae6bb
saba <sabaphruidze01@gmail.com>
2023-05-30 14:26:51 +0400

    added images

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-
+import batumi from "./assets/batumi.jpg";
+import hunhary from "./assets/hungary parliment.jpg";
+import moscow from "./assets/Moscow-city,_riverfront_view(31631549126).jpg";
+import paris from "./assets/paris.jpg";
+import taiwan from "./assets/taiwan.jpg";
+import viena from "./assets/viena.jpg";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div></div>
     </>
   );
 }
